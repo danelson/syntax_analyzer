00a96654964b03124d164579a5e58c22577e896a
Dan <danelson88@gmail.com>
2013-10-05 15:00:30 -0500

    fixed formatting

--- token.py
+++ token.py
@@ -7,55 +7,54 @@
 
 
 class Token:
-	'''
-	The Token class holds the type and value of each token from a Clite file.
-	'''
-	
-	def __init__(self, type=None, value=None):
-		'''
-		Initializes the Token class with a type and a value
-		@type: a string  (e.g. Identifier) 
-		@value: a string (e.g. xyz)
-		'''
-		self.type = type
-		self.value = value
-		
-		
-	def printToken(self):
-		'''
-		Prints the token's type and value
-		'''
-		print "Type: " + self.type + "\tValue: " + self.value
-		
-		
-	def compare(self, token):
-		'''
-		Compares 1 token against itself
-		@token: the token to compare against
-		@return: returns 2 if the tokens are the same type and value
-				 returns 1 if the tokens are the same but the types are
-					different
-				 returns 0 if the tokens are not the same
-		'''
-		if self.type == token.type:
-			if self.value == token.value:
-				return 2
-			return 1
-		return 0
-		
-		
-		
+    '''
+    The Token class holds the type and value of each token from a
+    Clite file.
+    '''
+    
+    def __init__(self, type=None, value=None):
+        '''
+        Initializes the Token class with a type and a value
+        @type: a string  (e.g. Identifier) 
+        @value: a string (e.g. xyz)
+        '''
+        self.type = type
+        self.value = value
+        
+    def printToken(self):
+        '''
+        Prints the token's type and value
+        '''
+        print "Type: {0}\tValue: {1}".format(self.type, self.value)
+        
+    def compare(self, token):
+        '''
+        Compares 1 token against itself
+        @token: the token to compare against
+        @return: 2 if tokens are the same type and value
+                 1 if tokens are the same but the types are
+                    different
+                 0 if the tokens are not the same
+        '''
+        if self.type == token.type:
+            if self.value == token.value:
+                return 2
+            return 1
+        return 0
+        
+        
+        
 if __name__ == "__main__":
-	token1 = Token("Variable", "int")
-	token2 = Token("Variable", "float")
-	token3 = Token("Variable", "int")
-	token1.printToken()
-	token2.printToken()
-	token3.printToken()
-	print token1.compare(token2)
-	print token1.compare(token3)
-	
-	
-	
-	
-		
+    token1 = Token("Variable", "int")
+    token2 = Token("Variable", "float")
+    token3 = Token("Variable", "int")
+    token1.printToken()
+    token2.printToken()
+    token3.printToken()
+    print token1.compare(token2)
+    print token1.compare(token3)
+    
+    
+    
+    
+        
